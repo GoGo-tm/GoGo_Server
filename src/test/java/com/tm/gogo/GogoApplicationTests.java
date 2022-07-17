4ed7b5019ce1cd40c24181397bb22797edf1757b
ParkJiwoon <bcp0109@gmail.com>
2022-07-17 16:13:35 +0900

    first commit

---
+++ src/test/java/com/tm/gogo/GogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tm.gogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GogoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
