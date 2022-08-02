66c9a1a79260b6684ebb75110a3b1025fd039191
ParkJiwoon <bcp0109@gmail.com>
2022-08-03 02:14:27 +0900

    feat: #3 Swagger 추가 (#18)

--- src/main/java/com/tm/gogo/web/auth/AuthController.java
+++ src/main/java/com/tm/gogo/web/auth/AuthController.java
@@ -1,6 +1,11 @@
 package com.tm.gogo.web.auth;
 
 import com.tm.gogo.domain.auth.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "/auth", description = "인증 API")
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
@@ -15,16 +21,31 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "가입하기")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "가입 성공하면 이메일 리턴"),
+            @ApiResponse(responseCode = "409", description = "이미 가입한 회원이 존재하는 경우", content = @Content)
+    })
     @PostMapping("/signup")
     public ResponseEntity<SignUpDto.Response> signUp(@RequestBody SignUpDto.Request signUpDto) {
         return ResponseEntity.ok(authService.signUp(signUpDto));
     }
 
+
+    @Operation(summary = "로그인")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "로그인 성공하면 토큰 리턴"),
+    })
     @PostMapping("/signin")
     public ResponseEntity<TokenDto.Response> signIn(@RequestBody SignInDto.Request memberRequestDto) {
         return ResponseEntity.ok(authService.signIn(memberRequestDto));
     }
 
+
+    @Operation(summary = "토큰 재발급")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Access Token 과 Refresh Token 을 재발급 해줌"),
+    })
     @PostMapping("/reissue")
     public ResponseEntity<TokenDto.Response> reissue(@RequestBody TokenDto.Request tokenRequestDto) {
         return ResponseEntity.ok(authService.reissue(tokenRequestDto));
