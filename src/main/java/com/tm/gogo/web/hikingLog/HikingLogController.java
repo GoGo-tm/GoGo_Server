4b0ca6cecfa970d68dd87b556494ad971fd0046d
syeonbot <asdf748596123@gmail.com>
2022-08-28 13:16:57 +0900

    feature: #28 HikingLog 컨트롤러 Swagger 추가

--- src/main/java/com/tm/gogo/web/hikingLog/HikingLogController.java
+++ src/main/java/com/tm/gogo/web/hikingLog/HikingLogController.java
@@ -5,6 +5,7 @@ import com.tm.gogo.hikingLog.HikingLogService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,9 +13,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "/hikinglog", description = "등산 기록")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/hikingLog")
+@RequestMapping("/api/hikinglog")
 public class HikingLogController {
     private final HikingLogService hikingLogService;
 
