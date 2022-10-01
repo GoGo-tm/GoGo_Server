ea370be196b4c182c9147d61f6a3d64657e31ed0
Songsiyeon <asdf748596123@gmail.com>
2022-10-02 06:23:40 +0900

    feat: #58 반환값에 이미지 추가 (#60)

--- src/main/java/com/tm/gogo/web/hiking_log/HikingLogResponse.java
+++ src/main/java/com/tm/gogo/web/hiking_log/HikingLogResponse.java
@@ -1,20 +1,26 @@
 package com.tm.gogo.web.hiking_log;
 
+import com.tm.gogo.domain.hiking_log.HikingLogImageMap;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.util.List;
 
 @Getter
 @ToString
-@NoArgsConstructor
 @AllArgsConstructor
 public class HikingLogResponse {
     private List<HikingLogDto> contents;
 
     @Schema(example = "true", description = "다음 데이터가 있는지 없는지 표시")
     private boolean hasNext;
+
+    public void setImageUrls(HikingLogImageMap hikingLogImageMap) {
+        contents.forEach(hikingLogDto -> {
+            List<String> urls = hikingLogImageMap.getImageUrls().get(hikingLogDto.getId());
+            hikingLogDto.setImageUrls(urls);
+        });
+    }
 }
