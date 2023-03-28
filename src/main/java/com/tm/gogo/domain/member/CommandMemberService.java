32cb64cc33f21df48624b2a7181e64c0b5db530c
sudo-dustle <asdf748596123@gmail.com>
2023-03-28 15:52:35 +0900

    feat: #90 캐시 설정

--- src/main/java/com/tm/gogo/domain/member/CommandMemberService.java
+++ src/main/java/com/tm/gogo/domain/member/CommandMemberService.java
@@ -2,6 +2,7 @@ package com.tm.gogo.domain.member;
 
 import com.tm.gogo.web.member.MemberRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,12 +16,14 @@ public class CommandMemberService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @CacheEvict(value = "Member", allEntries = true)
     public void updatePassword(String email, String newPassword) {
         Member member = queryMemberService.findByEmail(email);
         String encodedPassword = passwordEncoder.encode(newPassword);
         member.updatePassword(encodedPassword);
     }
 
+    @CacheEvict(value = "Member", allEntries = true)
     public void updateMember(Member member, MemberRequest memberRequest, String encodedNewPassword) {
         member.update(memberRequest.getNickname(), memberRequest.getEmail(), encodedNewPassword, memberRequest.isAgreed());
     }
@@ -29,6 +32,7 @@ public class CommandMemberService {
         return memberRepository.save(member);
     }
 
+    @CacheEvict(value = "Member", allEntries = true)
     public void delete(Long memberId) {
         memberRepository.deleteById(memberId);
     }
--- src/main/java/com/tm/gogo/domain/member/QueryMemberService.java
+++ src/main/java/com/tm/gogo/domain/member/QueryMemberService.java
@@ -3,6 +3,7 @@ package com.tm.gogo.domain.member;
 import com.tm.gogo.web.member.MemberResponse;
 import com.tm.gogo.web.response.ApiException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +18,7 @@ public class QueryMemberService {
 
     private final MemberRepository memberRepository;
 
+    @Cacheable(value = "Member")
     public MemberResponse findMemberInfoById(Long memberId) {
         return memberRepository.findById(memberId)
                 .map(MemberResponse::of)
