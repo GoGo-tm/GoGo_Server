c105115771f200d82fd849f4778bd5014708a659
Songsiyeon <asdf748596123@gmail.com>
2023-04-20 16:16:15 +0900

    feat: #90 Member 캐시 삭제 (#94)

--- src/main/java/com/tm/gogo/domain/member/CommandMemberService.java
+++ src/main/java/com/tm/gogo/domain/member/CommandMemberService.java
@@ -2,14 +2,11 @@ package com.tm.gogo.domain.member;
 
 import com.tm.gogo.web.member.MemberRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@CacheConfig(cacheNames = "Member")
 @Transactional
 @RequiredArgsConstructor
 public class CommandMemberService {
@@ -18,14 +15,12 @@ public class CommandMemberService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
 
-    @CacheEvict(allEntries = true)
     public void updatePassword(String email, String newPassword) {
         Member member = queryMemberService.findByEmail(email);
         String encodedPassword = passwordEncoder.encode(newPassword);
         member.updatePassword(encodedPassword);
     }
 
-    @CacheEvict(allEntries = true)
     public void updateMember(Member member, MemberRequest memberRequest, String encodedNewPassword) {
         member.update(memberRequest.getNickname(), memberRequest.getEmail(), encodedNewPassword, memberRequest.isAgreed());
     }
@@ -34,7 +29,6 @@ public class CommandMemberService {
         return memberRepository.save(member);
     }
 
-    @CacheEvict(allEntries = true)
     public void delete(Long memberId) {
         memberRepository.deleteById(memberId);
     }
