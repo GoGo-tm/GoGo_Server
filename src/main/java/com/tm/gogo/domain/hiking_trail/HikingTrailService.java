339763a9300cf1f42793b56b7e02568ddac7d6a7
sudo-dustle <asdf748596123@gmail.com>
2023-04-20 16:57:35 +0900

    feat: #95 HikingTrail 조회 캐시 적용

--- src/main/java/com/tm/gogo/domain/hiking_trail/HikingTrailService.java
+++ src/main/java/com/tm/gogo/domain/hiking_trail/HikingTrailService.java
@@ -8,6 +8,8 @@ import com.tm.gogo.web.hiking_trail.HikingTrailDto;
 import com.tm.gogo.web.hiking_trail.HikingTrailsResponse;
 import com.tm.gogo.web.response.ApiException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +19,7 @@ import java.util.stream.Collectors;
 import static com.tm.gogo.web.response.ErrorCode.HIKING_TRAIL_NOT_FOUND;
 
 @Service
+@CacheConfig(cacheNames = "hiking_trail")
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class HikingTrailService {
@@ -25,10 +28,12 @@ public class HikingTrailService {
     private final HikingTrailRepository hikingTrailRepository;
     private final FavoriteTrailService favoriteTrailService;
 
+    @Cacheable(key = "#condition + '::' + #scrollable", unless = "#result == null")
     public HikingTrailsResponse findHikingTrails(HikingTrailCondition condition, Scrollable scrollable) {
         return hikingTrailQueryRepository.findHikingTrails(condition, scrollable);
     }
 
+    @Cacheable(key = "#memberId + '::' + #condition + '::' + #scrollable", unless = "#result == null")
     public HikingTrailsResponse findHikingTrailsOfMember(Long memberId, HikingTrailCondition condition, Scrollable scrollable) {
         HikingTrailsResponse hikingTrails = hikingTrailQueryRepository.findHikingTrails(condition, scrollable);
         updateFavorite(memberId, hikingTrails);
@@ -47,10 +52,12 @@ public class HikingTrailService {
         hikingTrails.updateContentFavorites(favoriteTrailIds);
     }
 
+    @Cacheable(key = "'favorite::' + #memberId + '::' + #condition + '::' + #scrollable", unless = "#result == null")
     public HikingTrailsResponse findFavoriteHikingTrails(Long memberId, HikingTrailCondition condition, Scrollable scrollable) {
         return hikingTrailQueryRepository.findFavoriteHikingTrails(memberId, condition, scrollable);
     }
 
+    @Cacheable(key = "#hikingTrailId", unless = "#result == null")
     public HikingTrailDetailResponse findHikingTrail(Long hikingTrailId) {
         return hikingTrailRepository.findById(hikingTrailId)
                 .map(HikingTrailDetailResponse::of)
